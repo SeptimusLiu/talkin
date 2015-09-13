542656d7bdf4a393e0bb36b469a521dbda7296fa
yudan.lyd <yudan.lyd@alibaba-inc.com>
2015-09-13 20:45:04 +0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name talkinApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the talkinApp
+ */
+angular.module('talkinApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
