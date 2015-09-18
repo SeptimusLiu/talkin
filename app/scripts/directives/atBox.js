9b5bc65e580e9036f241dcc486f41d84a68de857
yudan.lyd <yudan.lyd@alibaba-inc.com>
2015-09-18 23:24:27 +0800

    add atBox popup

--- app/scripts/directives/atBox.js
+++ app/scripts/directives/atBox.js
@@ -1,15 +1,26 @@
 define(['directives/module'], function(directives) {
 	directives.directive('atBox', atBoxFunc);
+	atBoxFunc.$inject = ['$compile'];
 
-	function atBoxFunc() {
+	function atBoxFunc($compile) {
 		return {
 			require: 'ngModel',
 			restrict: 'A',
 			link: function (scope, element, attrs) {
+				var users = [];
+				var template = '<div class="popover top fade in" style="display: block; top: -50px; left: 0; width: 73.7344px; height: 62px;">\
+  											<div class="arrow"></div><div class="popover-inner">\
+  											<div class="popover-content"><div class="talkin-option-wrapper">\
+												<ul>\
+													<li ng-repeat="item in mainCtrl.users"><a href="javascript:;">{{item.name}}</a></li>\
+												</ul></div></div></div></div>';
 				
 				scope.$watch(attrs.ngModel, function (value) {
-					if (value && value.charAt[value.length - 1] == '@') {
-						console.log(items[0]);
+					element.siblings('.popover').remove();
+					if (value && value.charAt(value.length - 1) == '@') {
+						console.log('@');
+						element.before(template);
+						$compile(element.contents())(scope);
 					}
 				});
 			}
